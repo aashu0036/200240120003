66f60af008f3bc89b2262dbbc747bffb357824b1
Aashu <aashu0036@gmail.com>
2022-01-07 22:29:22 +0530

    Java commit test

---
+++ GitLabTest.java
@@ -0,0 +1,9 @@
+public class GitLabTest{
+    public static void print(){
+        System.out.println("Printing from static method");
+    }
+  
+    public static void main(String[] args) {
+        print();
+    }  
+}
